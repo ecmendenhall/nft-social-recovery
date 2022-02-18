aa17da17d5d0c324f3ec060ad355cb162c26c35d
Marco Gattello <44922848+mgattello@users.noreply.github.com>
2022-02-18 00:25:57 +0000

    add connect and disconnect buttons, add a bit of style

--- app/nft-social-recovery/src/App.tsx
+++ app/nft-social-recovery/src/App.tsx
@@ -1,22 +1,72 @@
-import React, { ReactElement } from "react";
-import "./App.css";
+import { ReactElement } from "react";
+import { Container, Button } from "@mui/material";
+import { Lock } from "@mui/icons-material";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from "@web3-react/core";
 
-const App: React.FC<ReactElement> = () => {
+const injected = new InjectedConnector({
+  supportedChainIds: [1],
+});
+
+const App: React.FC = (): ReactElement => {
+  // eslint-disable-next-line prettier/prettier
+  const { active, account, activate, deactivate } = useWeb3React();
+  const connect = async () => {
+    try {
+      await activate(injected);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const disconnect = async () => {
+    try {
+      deactivate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <Container
+          sx={{
+            display: "flex",
+            color: "white",
+            fontSize: 35,
+            marginBottom: 10,
+            padding: 2,
+            gap: 1,
+            bgcolor: "#282c34",
+            justifyContent: "center",
+            alignItem: "center",
+          }}
         >
-          Learn React
-        </a>
+          <div>NFT Social Recovery</div>
+          <Lock />
+        </Container>
       </header>
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItem: "center",
+        }}
+      >
+        {active ? (
+          <span>
+            Connected with <b>{account}</b>
+          </span>
+        ) : (
+          <Button variant="contained" onClick={connect}>
+            Connect Wallet
+          </Button>
+        )}
+        <Button variant="contained" onClick={disconnect}>
+          Disconnect
+        </Button>
+      </Container>
     </div>
   );
 };
--- app/nft-social-recovery/src/index.tsx
+++ app/nft-social-recovery/src/index.tsx
@@ -1,12 +1,20 @@
- import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// eslint-disable-next-line node/no-missing-import
+/* eslint-disable node/no-missing-import */
 import App from "./App";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+
+const getLibrary = (provider: any) => {
+  return new Web3(provider);
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
